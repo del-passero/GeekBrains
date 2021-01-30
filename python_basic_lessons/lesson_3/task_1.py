f320e32adcd979e196f5a66867c8a966cc63b0a3
Alexander Vorobyev <ap.vorobyev@gmail.com>
2021-01-30 12:13:20 +0300

    Задания 1-6 выполнены

--- python_basic_lessons/lesson_3/task_1.py
+++ python_basic_lessons/lesson_3/task_1.py
@@ -2,20 +2,22 @@
 1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
 Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
 '''
-def div(*args):
-    def enter_values(*args):
-        try:
-            dividend = int(input("Введите делимое число: "))
-            divider = int(input("Введите делитель числа: "))
-        except ValueError:
-            return 'Вы ввели не число'
-        return dividend, divider
+
+
+def div(a, b):
     try:
-        result = dividend / divider
+        a = int(input("Введите делимое: "))
+        b = int(input("Введите делитель: "))
+        result = a / b
+    except ValueError:
+        return 'Не получилось! Вы ввели не число!'
     except ZeroDivisionError:
-        return "На ноль делить нельзя"
+        return "Не получилось! На ноль делить нельзя!"
     return result
-
-
-
-div()
+a = 0
+b = 0
+question = None
+while question != "00":
+    result = print(f'Ваш результат: {div(a, b)}')
+    question = input("\n Хотите продолжить? Если да - нажмите Enter, если нет - введите '00' и программа закроется.")
+print("До свидания!")
